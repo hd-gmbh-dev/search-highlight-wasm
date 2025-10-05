1c384e8c303adc584df1337fbd68d0e19770d9c5
Seitz Jürgen - H & D <seitz.h-d@partner.akdb.de>
2025-10-05 11:27:57 +0200

    format code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use std::collections::BTreeMap;
 
-use tantivy::{schema::Field, snippet::SnippetGenerator, tokenizer::{LowerCaser, NgramTokenizer, RemoveLongFilter, TextAnalyzer}};
+use tantivy::{
+    schema::Field,
+    snippet::SnippetGenerator,
+    tokenizer::{LowerCaser, NgramTokenizer, RemoveLongFilter, TextAnalyzer},
+};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -10,8 +14,11 @@ pub fn highlight_ngram3(line: String, search: String) -> Result<String, JsValue>
         .filter(RemoveLongFilter::limit(150))
         .build();
     let snippet_generator = SnippetGenerator::new(
-        BTreeMap::from_iter([(search, 1.0)])
-    , tokenizer, Field::from_field_id(0), 150);
+        BTreeMap::from_iter([(search, 1.0)]),
+        tokenizer,
+        Field::from_field_id(0),
+        150,
+    );
     let html = snippet_generator.snippet(&line).to_html();
     if html.is_empty() {
         return Ok(line);
